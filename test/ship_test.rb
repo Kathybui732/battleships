d4911d99668ee47f044ba2da688b0c8a6f8ac99d
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-11 20:24:43 -0600

    Create initial tests

---
+++ test/ship_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require 'pry'
+
+class ShipTest < Minitest::Test
+
+  def setup
+    @cruiser = Ship.new("Cruiser", 3)
+  end
+
+  def test_it_exists
+    assert_instance_of Ship, @cruiser
+  end
+
+  def test_it_is_initialized_with_a_name
+    assert_equal "Cruiser", @cruiser.name
+  end
+
+  def test_it_has_a_length
+    assert_equal 3, @cruiser.length
+  end
+
+  def test_it_has_health
+    assert_equal 3, @cruiser.health
+  end
+
+  def test_sunk?
+    assert_equal false, @cruiser.sunk?
+  end
+end
