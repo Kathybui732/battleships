a616ee0381fc5361ced0118a556fcab2fe493643
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-12 17:05:24 -0600

    Create coordinate test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -2,6 +2,7 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/ship'
 require './lib/cell'
+require 'pry'
 
 class CellTest < Minitest::Test
 
@@ -11,7 +12,12 @@ class CellTest < Minitest::Test
   end
 
   def test_cell_exists
+    require "pry"; binding.pry
     assert_instance_of Cell, @cell
+  end
+
+  def test_it_has_a_coordinate
+    assert_equal "B4", @cell.coordinate
   end
 
 end
