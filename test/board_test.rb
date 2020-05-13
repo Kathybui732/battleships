dfc6c4a8ad4985d88b3df189e8cfa0f2117d82cf
Austin Aspaas <araspaas@live.com>
2020-05-13 16:33:20 -0600

    add test for valid coordinate

--- test/board_test.rb
+++ test/board_test.rb
@@ -18,4 +18,12 @@ class BoardTest < Minitest::Test
     assert_equal Hash, @board.cells.class
     assert_equal 16, @board.cells.count
   end
+
+  def test_is_has_valid_coordinate
+    assert @board.valid_coordinate?("A1")
+    assert @board.valid_coordinate?("D4")
+    refute @board.valid_coordinate?("A5")
+    refute @board.valid_coordinate?("E1")
+    refute @board.valid_coordinate?("A22")
+  end
 end
