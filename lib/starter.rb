7e123e76a5f937fec43f56c8cdd0676c3942c1f7
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-18 13:03:45 -0600

    Create methods to create board and place ships

--- lib/starter.rb
+++ lib/starter.rb
@@ -1,35 +1,46 @@
 class Starter
-  attr_reader :cpu_board, :player_board
-  def initialize
-    @cpu_board = Board.new
-    @player_board = Board.new
-    @cruiser_1 = Ship.new("Cruiser", 3)
-    @cruiser_2 = Ship.new("Cruiser", 3)
-    @sub_1 = Ship.new("Sub", 2)
-    @sub_2 = Ship.new("Sub", 2)
+  attr_reader :cpu_board, :player_board, :placement_1, :placement_2
+
+  def initialize(cpu_board, player_board)
+    @cpu_board = cpu_board
+    @player_board = player_board
+    @placement_1 = nil
+    @placement_2 = nil
   end
-    # Change create ship method name
-  def default_setup
-    @cpu_board.create_ship("sub", 2)
+
+  def calibrate_board_for_ships(length_1, length_2)
+    @cpu_board.create_board(length_1)
     @cpu_board.number_coordinates
     @cpu_board.letter_coordinates
-    @player_board.create_ship("sub", 2)
-    @player_board.number_coordinates
-    @player_board.letter_coordinates
-    @cpu_board.create_ship("cruiser", 3)
+    @cpu_board.create_board(length_2)
     @cpu_board.number_coordinates
     @cpu_board.letter_coordinates
     @cpu_board.ship_coordinates
-    @player_board.create_ship("cruiser", 3)
+    @player_board.create_board(length_1)
+    @player_board.number_coordinates
+    @player_board.letter_coordinates
+    @player_board.create_board(length_2)
     @player_board.number_coordinates
     @player_board.letter_coordinates
     @player_board.ship_coordinates
   end
 
-  # def cpu_place_ship(ship, length)
-  #   until @cpu_board.valid_placement? do
-  #     @cpu_board.valid_ship_coordinates.sample(1)
-  #     @cpu_board.valid_placement?
-  # end
+  def cpu_first_placement(ship, length)
+    first_placement = @cpu_board.valid_ship_coordinates.select do |array|
+      array.count == length
+    end
+    @placement_1 = first_placement.sample(1).flatten!
+    @cpu_board.place(ship, @placement_1)
+  end
+
+  def cpu_second_placement(ship, length)
+    second_placement = @cpu_board.valid_ship_coordinates.map do |array|
+      array.reject do |element|
+        element.include?(@placement_1[0]) || element.include?(@placement_1[1]) || element.include?(@placement_1[2])
+      end
+    end
+    @placement_2 = second_placement.sample(1).flatten!
+    @cpu_board.place(ship, @placement_2)
+  end
 
 end
