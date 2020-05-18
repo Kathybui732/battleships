2b7a60b2664826ba685509d4a9a317bc107f9e5a
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-18 13:04:32 -0600

    Pass all tests for board game setup

--- test/starter_test.rb
+++ test/starter_test.rb
@@ -8,24 +8,32 @@ require "pry"
 
 class StarterTest < Minitest::Test
   def setup
-    @start_game = Starter.new
+    @cpu_board = Board.new
+    @player_board = Board.new
+    @start_game = Starter.new(@cpu_board, @player_board)
+    @cruiser = Ship.new("Cruiser", 3)
+    @sub = Ship.new("Submarine", 2)
   end
 
   def test_it_exists
     assert_instance_of Starter, @start_game
   end
 
-  def test_it_can_create_default_setup
-    @start_game.default_setup
-    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", @start_game.cpu_board.render
-    assert_equal "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n", @start_game.player_board.render
+  def test_it_can_calibrate_board
+    expected = [["A1", "A2", "A3"], ["A2", "A3", "A4"], ["B1", "B2", "B3"], ["B2", "B3", "B4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["B1", "C1", "D1"], ["B2", "C2", "D2"], ["B3", "C3", "D3"], ["B4", "C4", "D4"], ["A1", "A2"], ["A2", "A3"], ["A3", "A4"], ["B1", "B2"], ["B2", "B3"], ["B3", "B4"], ["C1", "C2"], ["C2", "C3"], ["C3", "C4"], ["D1", "D2"], ["D2", "D3"], ["D3", "D4"], ["A1", "B1"], ["A2", "B2"], ["A3", "B3"], ["A4", "B4"], ["B1", "C1"], ["B2", "C2"], ["B3", "C3"], ["B4", "C4"], ["C1", "D1"], ["C2", "D2"], ["C3", "D3"], ["C4", "D4"]]
+    assert_equal expected.sort, @start_game.calibrate_board_for_ships(3, 2).sort
   end
 
-  def test_it_can_create_array_of_valid_coordinates
-    skip
-    @start_game.default_setup
-     expected = [["A1", "A2", "A3"], ["A2", "A3", "A4"], ["B1", "B2", "B3"], ["B2", "B3", "B4"], ["C1", "C2", "C3"], ["C2", "C3", "C4"], ["D1", "D2", "D3"], ["D2", "D3", "D4"], ["A1", "B1", "C1"], ["A2", "B2", "C2"], ["A3", "B3", "C3"], ["A4", "B4", "C4"], ["B1", "C1", "D1"], ["B2", "C2", "D2"], ["B3", "C3", "D3"], ["B4", "C4", "D4"], ["A1", "A2"], ["A2", "A3"], ["A3", "A4"], ["B1", "B2"], ["B2", "B3"], ["B3", "B4"], ["C1", "C2"], ["C2", "C3"], ["C3", "C4"], ["D1", "D2"], ["D2", "D3"], ["D3", "D4"], ["A1", "B1"], ["A2", "B2"], ["A3", "B3"], ["A4", "B4"], ["B1", "C1"], ["B2", "C2"], ["B3", "C3"], ["B4", "C4"], ["C1", "D1"], ["C2", "D2"], ["C3", "D3"], ["C4", "D4"]]
-     assert_equal expected.sort, @start_game.cpu_board.valid_ship_coordinates.sort
-     assert_equal expected.sort, @start_game.player_board.valid_ship_coordinates.sort
+  def test_it_can_place_first_ship
+    @start_game.calibrate_board_for_ships(3, 2)
+    @start_game.cpu_first_placement(@cruiser, 3)
+    assert_equal 3, @start_game.placement_1.count
+  end
+
+  def test_it_can_place_second_ship
+    @start_game.calibrate_board_for_ships(3, 2)
+    @start_game.cpu_first_placement(@cruiser, 3)
+    @start_game.cpu_second_placement(@sub, 2)
+    assert_equal 2, @start_game.placement_2.count
   end
 end
