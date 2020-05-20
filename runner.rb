c7f13b45fa66fad069157bab0290574b408d6f77
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-20 16:10:00 -0600

    Create option for custom version start

--- runner.rb
+++ runner.rb
@@ -2,10 +2,13 @@ require './lib/ship'
 require './lib/cell'
 require './lib/board'
 require './lib/starter'
+require './lib/custom_game'
 require 'pry'
 
 player_board = Board.new
 cpu_board = Board.new
 battleship = Starter.new(cpu_board, player_board)
+custom_game = CustomGame.new
 
 battleship.start
+# custom_game.start
